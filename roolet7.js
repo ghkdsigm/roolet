5567dae3a0bed6ef0e5e31ecc48061b8f12cf957
Hwangseunghyun <ghkdsigm3@gmail.com>
2022-12-12 12:23:02 +0900

    로그인 기능

--- roolet7.js
+++ roolet7.js
@@ -1,4 +1,25 @@
 $(function () {
+
+	var isLogin = true; //로그인일시 true, 로그아웃중일시 false
+	var $defaultButton = $('.slotMachinePrev')
+	var $startButton = $('.slotMachineStarting')
+
+	if(isLogin) {
+		$startButton.show()
+		$defaultButton.hide()
+		machineStart()
+	} else {
+		$startButton.hide()
+		$defaultButton.show()
+	}
+
+	$defaultButton.on('click', function() {
+		alert('로그인 후 참여 가능합니다.');
+		document.location.href = "https://www.itembay.com/login/loginAdult" ;
+		return;	
+	})
+
+	/* 슬롯 순환 실행 */
 	function slotRoller(spd, selector) {
 	  var speed1 =
 	    selector === '#slot_box1 .list'
@@ -51,8 +72,7 @@ $(function () {
 		  .animate({ left: '520px' }, calcSpeed2, 'linear', slotAfterAction)
 		  .promise()
 		  .done(function () {
-			$(this).addClass('on');
-  
+			$(this).addClass('on');  
 			slotMachineFinish(selector, spd);
 		  });
 	  }
@@ -61,33 +81,10 @@ $(function () {
 		  .animate({ left: '520px' }, calcSpeed3, 'linear', slotAfterAction)
 		  .promise()
 		  .done(function () {
-			$(this).addClass('on');
-  
+			$(this).addClass('on');  
 			slotMachineFinish(selector, spd);
 		  });
-	  }
-	  // $(selector)
-	  //   .animate(
-	  //     { left: '520px' },
-	  //     speed + (spd * 30 + spd),
-	  //     'linear',
-	  //     function () {
-	  //       // 첫째 목록을 마지막으로 이동
-	  //       firstChild = $('li:first-child', this);
-	  //       $(this).append(firstChild);
-	  //       $(this).css({ left: '0' });
-  
-	  //       // 현재 심볼 번호
-	  //       var symbol_no1 = $('li:nth-child(1)', '#slot_box1').attr('data-roll');
-	  //       $('#symbol_no1').text('symbol No:' + symbol_no1);
-	  //     }
-	  //   )
-	  //   .promise()
-	  //   .done(function () {
-	  //     $(this).addClass('on');
-  
-	  //     slotMachineStarting(selector);
-	  //   });
+	  }	 
 	}
   
 	/* 슬롯 객체를 정의 */
@@ -98,29 +95,24 @@ $(function () {
 		var dice2 = Math.floor(Math.random() * (16 - 9 + 1)) + 9;  //9~16중 랜덤 ( *돌아가는 횟수 포함 )
 		var dice3 = Math.floor(Math.random() * (24 - 17 + 1)) + 17;  //17~24중 랜덤 ( *돌아가는 횟수 포함 )
   
-		/* 무조건 당첨 */
+		/* 주사위확률 무조건 당첨 */
 		// var dice1 = 16;
 		// var dice2 = 16;
 		// var dice3 = 16;
+
 		if (v == 'dice1') {
 		  for (var i = 1; i <= dice1; i++) {
-			//console.log('a', dice1);
 			slotRoller(i, slotName);
-			//slotMachineFinish(dice1);
 		  }
 		}
 		if (v == 'dice2') {
 		  for (var i = 1; i <= dice2; i++) {
-			//console.log('b', dice2 - 8);
 			slotRoller(i, slotName);
-			//slotMachineFinish(dice2);
 		  }
 		}
 		if (v == 'dice3') {
 		  for (var i = 1; i <= dice3; i++) {
-			//console.log('c', dice3 - 16);
 			slotRoller(i, slotName);
-			//slotMachineFinish(dice3);
 		  }
 		}
 	  };
@@ -151,7 +143,7 @@ $(function () {
   
 	/* 결과 */
 	//레이어팝업:btn확인
-	//reset
+	//팝업리셋
 	$('.evtPop.build > div').hide();
 	$('.evtPop.build .btn.close').hide();
 	$('.btn.close').click(function () {
@@ -179,39 +171,41 @@ $(function () {
 	var slot_roll3 = new Slot_roll('#slot_box3 .list');
   
 	/* 슬롯 시작!! */
-	var slotCount = $('.slotCount');
-	count = 3;
-	slotCount.text(count);
-	$('.slotMachineStarting').on('click', function (e) {
-	  if (count === 0) {
-		e.preventDefault();
-		alert('플로린 코인이 부족합니다.');
+	function machineStart(){
+		var slotCount = $('.slotMachineStarting .slotCount');
+		count = 3;
 		slotCount.text(count);
-	  } else if (count >= 1) {
-		resetSlot();
-		$(this).addClass('disabled');
-		$('.slot .basicBg').removeClass('basicBg');
-		// $('.slot .basicBg').animate({opacity: "0"}, 500);
-		
-		var diceA = 'dice1';
-		var diceB = 'dice2';
-		var diceC = 'dice3';
+		$('.slotMachineStarting').on('click', function (e) {
+		if (count === 0) {
+			e.preventDefault();
+			alert('플로린 코인이 부족합니다.');
+			slotCount.text(count);
+		} else if (count >= 1) {
+			resetSlot();
+			$(this).addClass('disabled');
+			$('.slot .basicBg').removeClass('basicBg');
+			// $('.slot .basicBg').animate({opacity: "0"}, 500);
+			
+			var diceA = 'dice1';
+			var diceB = 'dice2';
+			var diceC = 'dice3';
 
-		// 애니메이션이 재생중이 아닐 때만 돌리고!!
-		if ($('#slot_box1 .list').is(':not(:animated)')) {
-		  slot_roll1.dice(diceA);
-		}
-		if ($('#slot_box2 .list').is(':not(:animated)')) {
-		  slot_roll2.dice(diceB);
-		}
-		if ($('#slot_box3 .list').is(':not(:animated)')) {
-		  slot_roll3.dice(diceC);
-		}
+			// 애니메이션이 재생중이 아닐 때만 돌리고!!
+			if ($('#slot_box1 .list').is(':not(:animated)')) {
+			slot_roll1.dice(diceA);
+			}
+			if ($('#slot_box2 .list').is(':not(:animated)')) {
+			slot_roll2.dice(diceB);
+			}
+			if ($('#slot_box3 .list').is(':not(:animated)')) {
+			slot_roll3.dice(diceC);
+			}
 
-		count--;
-		slotCount.text(count);
-	  }
-	});
+			count--;
+			slotCount.text(count);
+		}
+		});
+	}
   
 	/* 슬롯 연속해서 돌릴시 리셋 */
 	function resetSlot() {
