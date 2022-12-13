6dd2543b56456b8a6af0c4f18e7efd5ce1cb5f80
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-12-13 22:09:31 +0900

    뭐많이함

--- dk/dk.js
+++ dk/dk.js
@@ -1,3 +1,9 @@
+// 변수 count = 보유 코인 값 //
+// 변수 epromAA = 첫번째 슬롯 확률 값 //
+// 변수 epromBB = 첫번째 슬롯 확률 값 //
+// 변수 epromCC = 첫번째 슬롯 확률 값 //
+// 변수 isLogin = 로그인 여부 값 Boolean //
+
 $(function () {
   //슬롯 이미지
   var game1 = 'images/1.jpg';
@@ -24,9 +30,9 @@ $(function () {
   var epromB;
   var epromC;
 
-  var epromAA = 5;
-  var epromBB = 3;
-  var epromCC = 2;
+  var epromAA = 5; //확률 값 넣기
+  var epromBB = 3; //확률 값 넣기
+  var epromCC = 6; //확률 값 넣기
 
   //이미지 배열
   const arr = [
@@ -49,6 +55,7 @@ $(function () {
   var $defaultButton = $('.slotMachinePrev');
   var $startButton = $('.slotMachineStarting');
 
+  //로그인 여부
   if (isLogin) {
     $startButton.show();
     $defaultButton.hide();
@@ -59,11 +66,10 @@ $(function () {
   }
 
   var slotCount = $('.slotMachineStarting .slotCount');
-  count = 3;
+  count = 3; //보유 코인수
   slotCount.text('플로린 코인 ' + count + '개 소진');
 
   function machineStart() {
-    //button starts the spinning (official game start)
     $('.slotMachineStarting').on('click', function () {
       if (count === 0) {
         alert('플로린 코인이 부족합니다.');
@@ -74,46 +80,42 @@ $(function () {
         $('.slotMachineStarting').addClass('disabled');
         $('img').addClass('blur');
         $('img').removeClass('shake');
+        $('.reels').removeClass('on');
+        //bangDel($('.reels'));
         reelA.addClass('top2');
         reelB.addClass('top2');
         reelC.addClass('top3');
 
-        //this number stops the function once the max number condition has been met
+        // 첫번째 릴
         var counter = 0;
-        //function for slot one spin
-        var setIntervalIdA = setInterval(function slotOne() {
-          //counter adds one ever 10 milliseconds until it reaches 200
+        var setIntervalIdA = setInterval(function () {
           counter++;
-          //computer chooses a random number based on how many items are in array
           epromA = Math.floor(Math.random() * arr.length);
-          //epromA = 5
           //random number is the index of the array that given time (changes every 10 milliseconds)
           $(reelA).attr('src', arr[epromA]);
-          //console.log("spinning slot 1");
-          //Once the counter variable reaches 200, reel one stops and diplays the current image based on the index of the array
           if (counter === 140) {
-            //stopping the spin funcion
+            //회전 멈추는 시점
             $(reelA).attr('src', arr[epromAA - 1]);
             clearInterval(setIntervalIdA);
             console.log('slot stopped 1');
-            //displaying the current image in reel one
+            //현재 이미지 보여주는 시점
             console.log(epromA);
             $('#rollerA').removeClass('blur');
             $('#rollerA').addClass('shake');
+            $('#slotA').addClass('on');
             $('#rollerA').removeClass('top2');
+            bang($('#slotA'));
             return;
-            //end of all functions for reel one
           }
-          //this number determines how fast the reel spins (higher slower the RNG animation)
         }, 10);
-        //======================// refer to reel one for explanation
+
+        // 두번째 릴
         var counterB = 0;
-        var setIntervalIdB = setInterval(function slotTwo() {
+        var setIntervalIdB = setInterval(function () {
           counterB++;
           //epromB = Math.floor(Math.random() * arr.length);
           epromB = Math.floor(Math.random() * arr.length);
           $(reelB).attr('src', arr[epromB]);
-          //console.log("spinning slot 2");
           if (counterB === 220) {
             $(reelB).attr('src', arr[epromBB - 1]);
             clearInterval(setIntervalIdB);
@@ -121,19 +123,21 @@ $(function () {
             console.log(epromB);
             $('#rollerB').removeClass('blur');
             $('#rollerB').addClass('shake');
+            $('#slotB').addClass('on');
             $('#rollerB').removeClass('top2');
+            bang($('#slotB'));
             return;
           }
         }, 10);
-        //====================//
-        //***Reel 3 is compares wins in all variables and spits out score at return
+
+        // 세번째 릴
         var counterC = 0;
-        var setIntervalIdC = setInterval(function slotThree() {
+        var setIntervalIdC = setInterval(function () {
           counterC++;
           //epromC = Math.floor(Math.random() * arr.length);
           epromC = Math.floor(Math.random() * arr.length);
           $(reelC).attr('src', arr[epromC]);
-          //console.log("spinning slot 3");
+
           if (counterC === 400) {
             $(reelC).attr('src', arr[epromCC - 1]);
             clearInterval(setIntervalIdC);
@@ -141,25 +145,26 @@ $(function () {
             console.log(epromC);
             $('#rollerC').removeClass('blur');
             $('#rollerC').addClass('shake');
+            $('#slotC').addClass('on');
             $('#rollerC').removeClass('top3');
+            bang($('#slotC'));
+
             setTimeout(function () {
               $('.slotMachineStarting').removeClass('disabled');
             }, 1000);
             console.log(epromA + ' ' + epromB + ' ' + epromC);
 
-            //SCOREBOARD
-            //1. ***JACKPOT****
-            if (epromA == 13 && epromB == 13 && epromC == 13) {
+            //***3개 슬롯 다 맞을경우****
+            if (epromAA === epromBB && epromBB === epromCC) {
               setTimeout(success, 1000);
             } else {
               setTimeout(fail, 1000);
             }
             return;
-            //end of last spin reel
           }
         }, 10);
 
-        count--;
+        count--; //코인 수 줄이기
         slotCount.text('플로린 코인 ' + count + '개 소진');
       }
 
@@ -168,6 +173,13 @@ $(function () {
         $('.evtPop').hide();
         $('.evtPop.build > div').hide();
         $('.evtPop.build .btn.close').hide();
+      }
+
+      /* 폭죽태그 추가 */
+      function bang(ele) {
+        ele.append(
+          "<div class='pyro'><div class='after'></div><div class='before'></div></div>"
+        );
       }
 
       /* 결과 */
